c2c68240b42de32b4eb28f25ae6c97749527cf66
AAVolkov <AAVolkov@synergy.ru>
2017-07-27 18:10:41 +0300

    Расширяемый контроллер бэкенда в common

---
+++ common/controllers/DefaultBackendController.php
@@ -0,0 +1,15 @@
+<?php
+namespace common\controllers;
+
+/**
+ * Class DefaultBackendController
+ *
+ * Основа для бэкенд контроллеров модулей (все должны наследовать данный класс).
+ * Должен наследоваться от отсновного контроллера backend приложения.
+ *
+ * @package common\controllers
+ */
+class DefaultBackendController extends \PhpDevil\Extensions\Backend\components\DefaultBackendController
+{
+
+}
